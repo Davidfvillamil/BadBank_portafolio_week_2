c5f0261919ace0ab216781929281936484b5b39b
David Villamil <davidfvillamilm@gmail.com>
2023-09-24 21:53:02 -0500

    estilizar el home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,18 +5,25 @@ import { NavLink } from "react-router-dom";
 
 import '../Styles/home.css'
 
+
 const Home = () => {
     return (
         <div className="contanedor-externo">
+            <div className="video-background">
+                <video autoPlay loop muted>
+                    <source src='./images/home_video.mp4' type="video/mp4" />
+                    Tu navegador no soporta la reproducción de video.
+                </video>
+            </div>
             <div className="contendor-interno">
-                <h1 title="Titilo-home">Bienvenido a tu Banco</h1>
+                
                 <div className="imagen-banco-home"></div>
                 <div className="contenedor-botones-home">
                     <NavLink to={'/login'}>
-                        <button type="button" className="btn btn-primary">login</button>
+                        <button type="button" className="btn">login</button>
                     </NavLink>
                     <NavLink to={'/createaccount'}>
-                        <button type="button" className="btn btn-primary">Create Account</button>
+                        <button type="button" className="btn">Create Account</button>
                     </NavLink>
                 </div>
             </div>
